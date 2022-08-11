7dbfff1bd7ac92030531bd6cd8f28fe20d2b3c6a
Arth Tyagi <arthtyagi7@gmail.com>
2022-08-11 07:18:41 +0530

    remove about

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Outlet, Link, useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
