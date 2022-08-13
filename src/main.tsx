77fe846d13c59008a77c7139d9bb5d4a2e5ecb88
Arth Tyagi <arthtyagi7@gmail.com>
2022-08-13 16:53:08 +0530

    fix type error in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={new QueryClient()}>
       <BrowserRouter>
