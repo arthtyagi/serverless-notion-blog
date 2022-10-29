bb294491277b979d0a1e7064867731253a757077
Arth Tyagi <arthtyagi7@gmail.com>
2022-10-29 22:01:20 +0530

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
+/* eslint-disable no-restricted-globals */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { inject } from '@vercel/analytics';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { webVitals } from './vitals';
 import App from './App';
 import './index.css';
-import { sendToVercelAnalytics } from './vitals';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -17,4 +18,17 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   </React.StrictMode>,
 );
 
-reportWebVitals(sendToVercelAnalytics);
+const analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+
+if (analyticsId) {
+  webVitals({
+    path: location.pathname,
+    params: location.search,
+    analyticsId,
+    debug: import.meta.env.DEV,
+  });
+}
+
+if (import.meta.env.PROD) {
+  inject();
+}
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(
+      process.env.VERCEL_ANALYTICS_ID,
+    ),
+  },
   resolve: {
     alias: {
       'node-fetch': 'axios',
